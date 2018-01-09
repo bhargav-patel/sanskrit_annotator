63adc40f850f379c60d01e642bedb367c49813f1
Bhargavkumar Patel <bhargav079@gmail.com>
2018-01-09 12:36:02 +0530

    Added tooltips.

--- annotator/forms.py
+++ annotator/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class SearchForm(forms.Form):
-	word = forms.CharField(max_length=100,required=False)
-	sent_id = forms.IntegerField(required=False)
-	POS = forms.CharField(max_length=100,required=False)
-	dep = forms.CharField(max_length=100,required=False)
-	lemma = forms.CharField(max_length=100,required=False)
-	morph = forms.CharField(max_length=100,required=False)
-	color_class = forms.CharField(max_length=100,required=False)
-	level = forms.IntegerField(required=False)
+	word = forms.CharField(max_length=100,required=False,help_text="Partial or Full word case insensitive")
+	sent_id = forms.IntegerField(required=False,help_text="sentence id")
+	POS = forms.CharField(max_length=100,required=False,help_text="POS tag of word")
+	dep = forms.CharField(max_length=100,required=False,help_text="dependecy tag of relation between word and its parent")
+	lemma = forms.CharField(max_length=100,required=False,help_text="Words with options having this lemma")
+	morph = forms.CharField(max_length=100,required=False,help_text="Words with options having this morph")
+	color_class = forms.CharField(max_length=100,required=False,help_text="Words with options having this color_class")
+	level = forms.IntegerField(required=False,help_text="Words with options having this level")
